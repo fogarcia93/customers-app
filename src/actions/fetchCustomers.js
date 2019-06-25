4c9d9d3d7dd8b5c56cc67291599b98ad014375d4
fogarcia93 <fogarcia10@gmail.com>
2019-06-24 20:50:42 -0600

    video 179

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,9 +1,6 @@
 import { FETCH_CUSTOMERS } from "./../constants";
 import { createAction } from 'redux-actions';
-
-const url = 'http://localhost:3001/customers';
-
-const apiFetchCustomers = () => fetch(url).then(v => v.json());
-
+import { apiGet } from './../api';
+import { urlCustomers } from './../api/urls';
   
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiGet(urlCustomers));
