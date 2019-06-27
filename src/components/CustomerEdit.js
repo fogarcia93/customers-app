c7ba79e1c3795cb909d88bfa7f8c628fd97f7e0d
fogarcia93 <fogarcia10@gmail.com>
2019-06-26 21:50:09 -0600

    video 198

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,21 +1,81 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { reduxForm, Field } from "redux-form";
+import { setPropsAsInitial } from "./../helpers/setPropsAsInitial";
+import CustomersActions from './../components/CustomersActions';
 
-const CustomerEdit = ({name, dni, age}) => {
+// const isRequired = value => !value && "Este Campo es Requerido";
+
+const MyField = ({ input, meta, type, label, name }) => (
+  <div>
+    <label htmlFor={name}>{label}</label>
+    <input {...input} type={!type ? "text" : type} />
+    {meta.touched && meta.error && <span>{meta.error}</span>}
+  </div>
+);
+
+const isNumber = value => isNaN(Number(value)) && "El campo debe ser un número";
+
+const validate = values =>{
+  const error = {};
+
+  if (!values.name) {
+    error.name = "El campo nombre es requerido";
+  }
+  if (!values.dni) {
+    error.dni = "El dni es un campo obligatorio";
+  }
+
+  return error;
+};
+
+const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack}) => {
   return (
     <div>
       <h2>Edicion del cliente</h2>
       <h3>
-        Nombre: {name}/ Dni: {dni}/ Edad: {age}
+        <form onSubmit={handleSubmit} >
+          <Field
+          
+            name="name"
+            component={MyField}
+            label="Nombre"
+          />
+
+          <Field
+            name="dni"
+            component={MyField}
+            label="Dni"
+          />
+
+          <Field
+            name="age"
+            component={MyField}
+            type="number"
+            validate={isNumber}
+            label="Edad"
+          />
+          <CustomersActions >
+            <button type="submit" disabled={submitting} >Aceptar</button>
+            <button onClick={onBack} >Cancelar</button>
+          </CustomersActions>
+        </form>
       </h3>
     </div>
   );
 };
 
 CustomerEdit.propTypes = {
-    name: PropTypes.string,
-    dni: PropTypes.string,
-    age: PropTypes.number,
+  name: PropTypes.string,
+  dni: PropTypes.string,
+  age: PropTypes.number,
+  onBack: PropTypes.func.isRequired,
 };
 
-export default CustomerEdit;
+const CustomerEditForm = reduxForm(
+  {
+     form: "CustomerEdit",
+     validate
+  })(CustomerEdit);
+
+export default setPropsAsInitial(CustomerEditForm);
