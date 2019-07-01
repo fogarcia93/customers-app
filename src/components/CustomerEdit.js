70681df1cf25c1278ff6bc7695c934dc3b64b630
fogarcia93 <fogarcia10@gmail.com>
2019-06-30 18:04:59 -0600

    video 215

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { reduxForm, Field } from "redux-form";
 import { setPropsAsInitial } from "./../helpers/setPropsAsInitial";
@@ -7,14 +7,6 @@ import { Prompt } from 'react-router-dom';
 
 
 // const isRequired = value => !value && "Este Campo es Requerido";
-
-const MyField = ({ input, meta, type, label, name }) => (
-  <div>
-    <label htmlFor={name}>{label}</label>
-    <input {...input} type={!type ? "text" : type} />
-    {meta.touched && meta.error && <span>{meta.error}</span>}
-  </div>
-);
 
 const isNumber = value => isNaN(Number(value)) && "El campo debe ser un número";
 
@@ -40,45 +32,70 @@ const toLower = value => value && value.toLowerCase();
 const onlyGrow = (value, previousValue, values) =>
   value && (!previousValue ? value : (value > previousValue ? value : previousValue));
 
-const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack, pristine, submitSucceeded }) => {
-  return (
+class CustomerEdit extends Component {
+
+  componentDidMount() {
+    if (this.txt) {
+      this.txt.focus();
+    }
+  }
+  
+
+   renderField = ({ input, meta, type, label, name, withFocus}) => (
     <div>
-      <h2>Edicion del cliente</h2>
-      <h3>
-        <form onSubmit={handleSubmit}>
-          <Field
-            name="name"
-            component={MyField}
-            label="Nombre"
-            parse={toUpper}
-            format={toLower}
-          />
-
-          <Field name="dni" component={MyField} label="Dni" />
-
-          <Field
-            name="age"
-            component={MyField}
-            type="number"
-            validate={isNumber}
-            label="Edad"
-            parse={toNumber}
-            normalize={onlyGrow}
-          />
-          <CustomersActions>
-            <button type="submit" disabled={pristine || submitting}>
-              Aceptar
-            </button>
-            <button type="button" disabled={submitting} onClick={onBack}>Cancelar</button>
-          </CustomersActions>
-          <Prompt
-          when={!pristine && !submitSucceeded }
-          message="Se perderan los datos si continua"
-          ></Prompt>
-        </form>
-      </h3>
+      <label htmlFor={name}>{label}</label>
+      <input {...input} type={!type ? "text" : type} 
+      ref={withFocus && (txt => this.txt =txt) } 
+      />
+      {meta.touched && meta.error && <span>{meta.error}</span>}
     </div>
   );
+  render() {
+    const  { handleSubmit, submitting, onBack, pristine, submitSucceeded } = this.props;
+    return (
+      <div>
+        <h2>Edicion del cliente</h2>
+        <h3>
+          <form onSubmit={handleSubmit}>
+            <Field
+              withFocus
+              name="name"
+              component={this.renderField}
+              label="Nombre"
+              parse={toUpper}
+              format={toLower}
+            />
+
+            <Field
+             name="dni" 
+             component={this.renderField} 
+             label="Dni" />
+
+            <Field
+              name="age"
+              component={this.renderField}
+              type="number"
+              validate={isNumber}
+              label="Edad"
+              parse={toNumber}
+              normalize={onlyGrow}
+            />
+            <CustomersActions>
+              <button type="submit" disabled={pristine || submitting}>
+                Aceptar
+                </button>
+              <button type="button" disabled={submitting} onClick={onBack}>Cancelar</button>
+            </CustomersActions>
+            <Prompt
+              when={!pristine && !submitSucceeded}
+              message="Se perderan los datos si continua"
+            ></Prompt>
+          </form>
+        </h3>
+      </div>
+    );
+  }
+
 };
 
 CustomerEdit.propTypes = {
--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,9 +1,44 @@
 import { handleActions } from 'redux-actions';
-import { FETCH_CUSTOMERS } from './../constants/index';
+import { FETCH_CUSTOMERS, INSERT_CUSTOMERS, UPDATE_CUSTOMERS } from './../constants/index';
 
 
 export const customers = handleActions({
-    [FETCH_CUSTOMERS]: (state, action) => [ ...action.payload],
+    [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
+    [INSERT_CUSTOMERS]: (state, action) => [...state, action.payload],
+    [UPDATE_CUSTOMERS]: (state, action) => {
+         const customerPayload = action.payload;
+         const { id } = customerPayload; // id= 2 name: 'nuevo nombre'
+        // [ { id: 1, name: '', ... },
+        //   { id: 2, name: 'viejo nombre', ... }, 
+       //    { id: 3, name: '', ... }]
+
+       const customers = state;
+       const initialValue = [];
+       // Primera Iteracion
+       // acc = []
+       // { id: 1, name: '', ... }
+       // [ { id: 1, name: '', ... } ]
+
+       //Segunda Iteracion
+       // acc = [ { id: 1, name: '', ... } ]
+       // { id: 2, name: 'viejo nombre', ... }
+       //  { id: 2, name: 'viejo nombre', ... } =>  { id: 2, name: 'nuevo nombre', ... }
+       // [ { id: 1, name: '', ... }, { id: 2, name: 'nuevo nombre', ... } ]
+
+       //Tercera Iteracion
+       // acc =  [ { id: 1, name: '', ... }, { id: 2, name: 'nuevo nombre', ... } ]
+       // { id: 3, name: '', ... }
+       // [ { id: 1, name: '', ... }, { id: 2, name: 'nuevo nombre', ... }, { id: 3, name: '', ... } ]
+       const newCustomers = customers.reduce((acc, customer) => {
+           if (customer.id === id) {
+               return[...acc, customerPayload];
+           }else{
+               return [...acc, customer]
+           }
+       }, initialValue)
+       
+       return newCustomers;
+    }
 }, []);
 
 
